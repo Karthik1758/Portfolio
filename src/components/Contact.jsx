7db2157ca24987fb6d6ad9881a31d0709ebf0a65
Karthik Chakali <karthik@pixelvide.com>
2025-04-17 13:30:43 +0530

    email updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <h2 className="section-heading">Contact Me</h2>
       <div className="flex flex-col items-center gap-4">
         <p className="flex items-center gap-2 text-lg">
-          <FaEnvelope /> chakalikarthik2001@gmail.com
+          <FaEnvelope /> chakalikarthik1858@gmail.com
         </p>
         <p className="flex items-center gap-2 text-lg">
           <FaPhone /> +91 6281506295
